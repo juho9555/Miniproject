0d10b0419a4c48aeb0d04945ffaac671dde04a05
JUHO <ijuho9555@gmail.com>
2025-08-14 15:41:58 +0900

    혼잡도 분류 구문 추가
    0~2명일 경우 여유
    3~5명일 경우 보통
    5명 이상일 경우 혼잡

--- src/tests.py/yolocam.py
+++ src/tests.py/yolocam.py
@@ -14,10 +14,21 @@ while True:
         # 사람 수 카운트하기
         count = len(results[0].boxes)
         cv2.putText(annotated_frame, f'People: {count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # 혼잡도 분류하기
+        if count <= 2:
+            cv2.putText(annotated_frame, f'not crowded', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+        elif count <= 5:
+            cv2.putText(annotated_frame, f'moderate crowded', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+        
+        else:
+            cv2.putText(annotated_frame, f'crowded', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            
         cv2.imshow('Yolo ', annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+
 cap.release()
 cv2.destroyAllWindows()
