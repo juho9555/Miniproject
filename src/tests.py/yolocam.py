defb862f046619cd0c987686d24a64ba790faeb8
JUHO <ijuho9555@gmail.com>
2025-08-14 15:02:56 +0900

    사람만 인식하고 화면에 몇명 확인되는지 표시

--- src/tests.py/yolocam.py
+++ src/tests.py/yolocam.py
@@ -8,10 +8,16 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     if ret:
-        results = model(frame, verbose= False)
+        results = model(frame, classes=[0], verbose= False) # 사람(class=0)만 탐지
         annotated_frame = results[0].plot()
+
+        # 사람 수 카운트하기
+        count = len(results[0].boxes)
+        cv2.putText(annotated_frame, f'People: {count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.imshow('Yolo ', annotated_frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    
 cap.release()
 cv2.destroyAllWindows()
