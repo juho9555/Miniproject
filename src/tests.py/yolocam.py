e7ae16e8098b4aa01354632e38be2da4ace1298d
JUHO <ijuho9555@gmail.com>
2025-08-14 17:16:56 +0900

    혼잡도를 status라는 변수로 지정

--- src/tests.py/yolocam.py
+++ src/tests.py/yolocam.py
@@ -42,14 +42,18 @@ while True:
 
         # 혼잡도 분류하기
         if count <= 2:
-            cv2.putText(annotated_frame, f'not crowded', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            status = 'not crowded'
+            cv2.putText(annotated_frame, status, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
         elif count <= 5:
-            cv2.putText(annotated_frame, f'moderate crowded', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            status = 'moderate crowded'
+            cv2.putText(annotated_frame, status, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         
         else:
-            cv2.putText(annotated_frame, f'crowded', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            status = 'crowded'
+            cv2.putText(annotated_frame, status, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             
+        update_html(count, status)
         cv2.imshow('Yolo ', annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
